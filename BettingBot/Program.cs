b760166feac53766fb273e2de1bc8713632cb21a
sasha <sasha@SAN4O>
2024-08-17 17:37:02 +0300

    Добавьте файлы проекта.

---
+++ BettingBot/Program.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Data;
+
+namespace BettingBot;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MainAsync(args).GetAwaiter().GetResult();
+    }
+
+    private static async Task MainAsync(string[] args)
+    {
+        var options = new ChromeOptions();
+
+        var driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl("https://zooma26.casino/");
+        Thread.Sleep(2000);
+
+        // Логика авторизации
+        SignIn(driver);
+
+        // Переход в зону ставок
+        driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/div[2]/div[1]/a[2]")).Click();
+        Thread.Sleep(3000);
+
+        // Бесконечный цикл для выполнения ставок
+        while (true)
+        {
+            // Выполнение ставки
+            PlaceBetWhenPossible(driver, 1);
+        }
+    }
+
+    private static void SignIn(ChromeDriver driver)
+    {
+        driver.FindElement(By.ClassName("rightBlockNotAuthTextDown")).Click();
+        Thread.Sleep(2000);
+
+        // Через Яндекс
+        driver.FindElement(By.XPath("/html/body/div[2]/div[6]/div[3]/a[3]")).Click();
+        Thread.Sleep(2000);
+
+        // Другие способы входа
+        driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/form/div[2]/button")).Click();
+        Thread.Sleep(2000);
+
+        // Почта
+        var login = "sashaoglashevichi@gmail.com";
+
+        // Поле для вставки почты
+        var loginField = driver.FindElement(By.XPath("//*[@id=\"passp-field-login\"]"));
+        loginField.SendKeys(login);
+        loginField.Click();
+        Thread.Sleep(2000);
+
+        // Кнопка войти
+        driver.FindElement(By.XPath("//*[@id=\"passp:sign-in\"]")).Click();
+        Thread.Sleep(2000);
+
+        // Кнопка отправить письмо для входа
+        driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/form/div/div[3]/div/button")).Click();
+        Thread.Sleep(2000);
+    }
+
+    private static void PlaceBetWhenPossible(ChromeDriver driver, int betAmount)
+    {
+        // Проверяем, активна ли кнопка
+        var betButton = driver.FindElement(By.Id("newBetApiGray"));
+
+        // Ожидание, пока кнопка станет неактивной (начало вращения)
+        while (betButton.GetAttribute("disabled") == null)
+        {
+            Thread.Sleep(500); // Ждем 500 мс перед проверкой снова
+        }
+
+        // Ожидание окончания вращения (кнопка снова станет активной)
+        while (betButton.GetAttribute("disabled") != null)
+        {
+            Thread.Sleep(500); // Ждем 500 мс перед проверкой снова
+        }
+
+        // Теперь кнопка активна, можно делать ставку
+        driver.FindElement(By.XPath("//*[@id=\"betAmount\"]")).Clear();
+        driver.FindElement(By.XPath("//*[@id=\"betAmount\"]")).SendKeys($"{betAmount}");
+
+        // Нажимаем на кнопку для ставки
+        betButton.Click();
+
+        // Небольшая задержка после ставки, чтобы не было множественных нажатий
+        Thread.Sleep(1000);
+    }
+}
---
+++ BettingStrategy/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace BettingStrategy;
+
+public class Program
+{
+    static int balance = 10000;
+    static int stavka = 10;
+    static int wins = 0;
+    static void Main(string[] args)
+    {
+        //            "PINK",
+        //            "BLUE",
+        //            "GRAY",
+        //            "ORANGE",
+
+        List<string> colors = new List<string>()
+        {
+
+            "PINK","BLUE","GRAY","BLUE","BLUE","BLUE","GRAY","PINK","BLUE","BLUE","PINK", // 912724
+            "BLUE","GRAY","BLUE","BLUE","BLUE","GRAY","PINK","BLUE","BLUE","PINK","PINK", // 912735
+            "PINK","PINK","BLUE","GRAY","GRAY","BLUE","BLUE","GRAY","GRAY","GRAY","BLUE", // 912746
+            "BLUE","BLUE","GRAY","PINK","ORANGE","BLUE","PINK","GRAY","BLUE","GRAY","GRAY", // 912757
+            "GRAY","GRAY","BLUE","BLUE","GRAY","PINK","GRAY","GRAY","BLUE","ORANGE","GRAY", // 912768
+            "GRAY","BLUE","GRAY","GRAY","GRAY","BLUE","BLUE","GRAY","GRAY","BLUE","GRAY", // 912779
+            "BLUE","GRAY","BLUE","GRAY","PINK","GRAY","GRAY","BLUE","GRAY","PINK","GRAY", // 912790
+            "GRAY","GRAY","GRAY","PINK","GRAY","BLUE","BLUE","PINK","BLUE","PINK","GRAY", // 912801
+            "PINK","PINK","PINK","GRAY","PINK","BLUE","BLUE","BLUE","GRAY","GRAY","GRAY", // 912812
+            "BLUE","BLUE","BLUE","ORANGE","GRAY","GRAY","BLUE","BLUE","GRAY","GRAY","PINK", // 912823
+            "ORANGE","PINK","GRAY","BLUE","GRAY","BLUE","BLUE","PINK","PINK","BLUE","BLUE", // 912834
+            "GRAY","GRAY","BLUE","GRAY","PINK","BLUE","BLUE","PINK","BLUE","GRAY","BLUE", // 912845
+            "BLUE","GRAY","BLUE","GRAY","PINK","GRAY","BLUE","BLUE","BLUE","BLUE","BLUE", // 912856
+            "GRAY","BLUE","PINK","GRAY","GRAY","BLUE","BLUE","PINK","BLUE","GRAY","GRAY", // 912867
+            "PINK","BLUE","BLUE","ORANGE","PINK","PINK","BLUE","GRAY","PINK","BLUE","BLUE", // 912878
+
+        };
+
+        //var colorCounts = colors
+        //   .GroupBy(c => c)
+        //   .Select(group => new { Color = group.Key, Count = group.Count() });
+
+        //foreach (var color in colorCounts)
+        //{
+        //    Console.WriteLine($"Цвет '{color.Color}' встречается {color.Count} раз(а)");
+        //}
+
+        int id = 1;
+        foreach (string color in colors)
+        {
+            if (balance > 0)
+                Console.WriteLine($"Id: {id} Balance: {balance} Stavka: {stavka} {Stavka(color)}");
+            else
+                return;
+
+            id++;
+        }
+
+        Console.WriteLine($"Final balance {balance}");
+        Console.WriteLine($"Total wins: {wins}");
+    }
+
+    static string Stavka(string color)
+    {
+        if (color == "GRAY")
+        {
+            wins++;
+            balance += stavka * 2;
+            int win = stavka;
+            stavka = 10;
+            return $"| Win: {win * 2}";
+        }
+        else
+        {
+            balance -= stavka;
+            int lose = stavka;
+            stavka *= 2;
+            return $"| Lose: {lose}";
+        }
+    }
+}
